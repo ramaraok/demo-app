59d54fad12fb486f89b3bda71c45cbb2f978befc
RamaRaoK <ramarao.kuppanadi@celigo.com>
2023-06-20 13:31:48 -0700

    updating demo app

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("ramaraok is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("ramaraok is Awesome!!! true", "larry3d", "yellow", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
